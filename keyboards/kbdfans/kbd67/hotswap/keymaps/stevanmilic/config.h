3c3cded92b9b7986e840c80bc7432cb8b644dd29
Stevan Milic <stevan.milic@yahoo.com>
2020-06-08 20:14:45 +0200

    Update config and rules in KBD67 keymap

--- keyboards/kbdfans/kbd67/hotswap/keymaps/stevanmilic/config.h
+++ keyboards/kbdfans/kbd67/hotswap/keymaps/stevanmilic/config.h
@@ -2,18 +2,21 @@
 
 #define FORCE_NKRO
 
-#define MOUSEKEY_DELAY             50
-#define MOUSEKEY_INTERVAL          15
-#define MOUSEKEY_MAX_SPEED         4
-#define MOUSEKEY_TIME_TO_MAX       50
-#define MOUSEKEY_WHEEL_MAX_SPEED   1
+#define MOUSEKEY_DELAY             0
+#define MOUSEKEY_INTERVAL          10
+#define MOUSEKEY_MAX_SPEED         3
+#define MOUSEKEY_TIME_TO_MAX       150
+#define MOUSEKEY_WHEEL_DELAY       0
+#define MOUSEKEY_WHEEL_INTERVAL    50
+#define MOUSEKEY_WHEEL_MAX_SPEED   3
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 50
-
-#define NO_ACTION_FUNCTION
-#define NO_ACTION_MACRO
-#define NO_ACTION_ONESHOT
 
 #define PERMISSIVE_HOLD
 #define TAPPING_TERM        200
-#define TAPPING_TOGGLE      2
+#define TAPPING_TOGGLE      3
 #define TAP_HOLD_CAPS_DELAY 50
+
+#undef  LOCKING_SUPPORT_ENABLE
+#define NO_ACTION_FUNCTION
+#define NO_ACTION_MACRO
+#define NO_ACTION_ONESHOT
